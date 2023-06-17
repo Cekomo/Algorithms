ebf9359e64d839af40fbe5474464df1fd051c016
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-06-17 18:44:50 +0300

    implement bubble sort algorithm

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,8 +2,19 @@
 
 BubbleSort::BubbleSort() = default;
 
-void BubbleSort::SortByAscending(int *elements, int) {
-    
+void BubbleSort::SortByAscending(int elements[], int size) {
+    for (int i = 0; i < size; i++) {
+        int j = 0;
+        while (j < size - i - 1) {
+            if (elements[j] > elements[j+1]) {
+                int valueAtJ = elements[j];
+                elements[j] = elements[j+1];
+                elements[j+1] = valueAtJ;
+            }
+
+            j++;
+        }
+    }
 }
 
 BubbleSort::~BubbleSort() = default;
