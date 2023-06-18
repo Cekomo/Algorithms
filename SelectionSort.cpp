6a4bb88060c3bf656c459f41df13714071b5bca1
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-06-18 21:25:14 +0300

    test selection sort algorithm

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -2,23 +2,23 @@
 
 SelectionSort::SelectionSort() = default;
 
-//void SelectionSort::SortByAscending(int elements[], int size) {
-//    for (int i = 0; i < size; i++) {
-//        int j = 0;
-//        int greatestElement = elements[j];
-//        int greatestElementIndex = 0;
-//        while (j < size - i - 1) {
-//            if (elements[j+1] > greatestElement) {
-//                greatestElement = elements[j+1];
-//                greatestElementIndex = j + 1;
-//            }
-//            j++;
-//        }
-//
-//        elements[greatestElementIndex] = elements[size-i-1];
-//        elements[size-i-1] = greatestElement;
-//    }
-//}
+void SelectionSort::SortByAscending(int elements[], int size) {
+    for (int i = 0; i < size; i++) {
+        int j = 0;
+        int greatestElement = elements[j];
+        int greatestElementIndex = 0;
+        while (j < size - i - 1) {
+            if (elements[j+1] > greatestElement) {
+                greatestElement = elements[j+1];
+                greatestElementIndex = j + 1;
+            }
+            j++;
+        }
+
+        elements[greatestElementIndex] = elements[size-i-1];
+        elements[size-i-1] = greatestElement;
+    }
+}
 
 //void SelectionSort::SortByAscending(int elements[], int size) {
 //    for (int i = 0; i < size; i++) {
@@ -37,25 +37,6 @@ SelectionSort::SelectionSort() = default;
 //        elements[i] = smallestElement;
 //    }
 //}
-
-void SelectionSort::SortByAscending(int elements[], int size) {
-    for (int i = 0; i < size; i++) {
-        int j = i; // Start comparing from the i-th position
-        int greatestElement = elements[j];
-        int greatestElementIndex = j;
-        while (j < size) {
-            if (elements[j] > greatestElement) {
-                greatestElement = elements[j];
-                greatestElementIndex = j;
-            }
-            j++;
-        }
-
-        elements[greatestElementIndex] = elements[size - 1];
-        elements[size - 1] = greatestElement;
-        size--; // Reduce the size of the unsorted portion
-    }
-}
 
 
 SelectionSort::~SelectionSort() = default;
--- main.cpp
+++ main.cpp
@@ -49,15 +49,15 @@ int main() {
 
     auto startTime = std::chrono::steady_clock::now();
 //    InsertionSort::SortByAscending(elements, size);
-//    BubbleSort::SortByAscending(elements, size);
-    SelectionSort::SortByAscending(elements,size);
+    BubbleSort::SortByAscending(elements, size);
+//    SelectionSort::SortByAscending(elements,size);
     auto endTime = std::chrono::steady_clock::now();
 
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 
     // Print variables
-    for (int i = 0; i < size; i++)
-        std::cout << elements[i] << " ";
+//    for (int i = 0; i < size; i++)
+//        std::cout << elements[i] << " ";
 
     std::cout << std::endl;
     std::cout << "Time taken by algorithm: " << duration.count() << " milliseconds.";
