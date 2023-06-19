b02b0cdc9097574085caa40bdbd11e730f37e95a
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-06-19 15:38:46 +0300

    split array and insert values

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -2,8 +2,23 @@
 
 MergeSort::MergeSort() = default;
 
-void MergeSort::SortByAscending(int *elements, int size) {
+void MergeSort::SortByAscending(int elements[], int size) {
+    int firstArraySize = size / 2;
+    int secondArraySize = size - firstArraySize;
+
+    int firstArray[firstArraySize];
+    int secondArray[secondArraySize];
+
+    for (int i = 0; i < size; i++) {
+        if (i < firstArraySize)
+            firstArray[i] = elements[i];
+        else
+            secondArray[i - firstArraySize] = elements[i];
+    }
     
+    //...
+//    MergeSort::SortByAscending(firstArray, firstArraySize);
+//    MergeSort::SortByAscending(secondArray, firstArraySize);
 }
 
 MergeSort::~MergeSort() = default;
