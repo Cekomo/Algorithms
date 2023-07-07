866e8f3ba2142ffa5385b1ed2a76972cf9d21fd7
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-07-07 17:04:14 +0300

    fix bugs on out of index & finding first element

--- JumpSearch.cpp
+++ JumpSearch.cpp
@@ -7,13 +7,11 @@ int JumpSearch::GetIndexFromJumpSearch(int value, const int *array, int size) {
     int jumpingIndex = sqrt(size);
 
     int i = 0;
+    if (array[i] == value) return 0;
     while (i < size && array[i] < value)
         i += jumpingIndex;
 
-
-//    for (int j = 0; j < jumpingIndex && (i - j) >= 0; j++)
-    for (int j = jumpingIndex - 1; j >= 0 && (i - j) >= 0; j--)
-    {
+    for (int j = jumpingIndex - 1; j >= 0 && (i - j) >= 0 && (i - j) < size; j--) {
         if (array[i - j] == value)
             return i - j;
     }
