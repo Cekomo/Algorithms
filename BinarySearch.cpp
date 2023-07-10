6c9183bb0eed4dcf67407755bae84701a34fb13a
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-07-10 15:28:19 +0300

    consider preceding same value elements

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,20 +6,24 @@ int BinarySearch::GetIndexFromBinarySearch(int value, const int *array, int size
     int left = 0;
     int right = size - 1;
     int mid = (left + right) / 2;
+    int result = -1;
 
-    while (array[mid] != value && (right - left) > 0) {
-        if (array[mid] > value) {
+    while (left <= right) {
+        if (array[mid] == value) {
+            // we do not return directly after finding the element to seek first occurrence..
+            //..if there is any copy of the element equal to "value"
+            result = mid;
             right = mid - 1;
         }
-        else if (array[mid] < value) {
+        else if (array[mid] < value)
             left = mid + 1;
-        }
+        else
+            right = mid - 1;
 
         mid = (left + right) / 2;
     }
 
-    if (array[mid] == value) return mid;
-    else return -1;
+    return result;
 }
 
 BinarySearch::~BinarySearch() {
