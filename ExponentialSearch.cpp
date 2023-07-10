9e52166c0939ae3b036b653eb6bce3d62c957a27
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-07-10 22:14:53 +0300

    handle zero index problem

--- ExponentialSearch.cpp
+++ ExponentialSearch.cpp
@@ -3,28 +3,26 @@
 ExponentialSearch::ExponentialSearch() = default;
 
 int ExponentialSearch::GetElementIndex(int value, const int *array, int size) {
-    int curveIndex = 0;
-    int currentJumpStep = 0;
-    int previousJumpStep;
+    if (array[0] == value) return 0;
+    int currentJumpStep = 1;
 
-    while (array[currentJumpStep] < value) {
-        previousJumpStep = currentJumpStep;
-        currentJumpStep = static_cast<int>(std::pow(2, ++curveIndex));
-        if (currentJumpStep > size) {
+    while (array[currentJumpStep] <= value) {
+        if (currentJumpStep * 2 < size) {
+            currentJumpStep *= 2;
+        }
+        else {
             currentJumpStep = size;
             break;
         }
     }
 
-    int left = previousJumpStep;
+    int left = currentJumpStep / 2;
     int right = currentJumpStep - 1;
     int mid = (left + right) / 2;
     int result = -1;
 
     while (left <= right) {
         if (array[mid] == value) {
-            // we do not return directly after finding the element to seek first occurrence..
-            //..if there is any copy of the element equal to "value"
             result = mid;
             right = mid - 1;
         }
